01d19e301eddf0df28826e0a3ffbb6dc6094d7a8
NaPann <napan.np@gmail.com>
2024-11-11 11:44:38 +0700

    cached

--- TESTWEB/bin/Debug/net8.0/init.sh
+++
@@ -1,6 +0,0 @@
-﻿# Filename init.sh used as entrypoint, put it in your .net core root # folder
-mkdir -p $HOME/.pki/nssdb
-certutil -d sql:$HOME/.pki/nssdb -N --empty-password
-pk12util -i Cert/empty-certificate.p12 -d sql:$HOME/.pki/nssdb -W 'YOURPASSWORD'
-Xvfb :99 -screen 0 640x480x8 -nolisten tcp &
-dotnet Selenium.Docker.Certificate.dll
